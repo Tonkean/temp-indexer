946b7a386878a4afdff915d75032c949826d40ee
Offir Talmor <talmor@tonkean.com>
2019-06-19 22:26:03 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,3 @@
+const Indexer = require('./indexer');
+
+Indexer.main();
---
+++ indexer.js
@@ -0,0 +1,12 @@
+const console = require('console');
+
+class Indexer {
+    constructor() {
+    }
+
+    main() {
+        console.debug('TEST');
+    }
+}
+
+module.exports = new Indexer();
